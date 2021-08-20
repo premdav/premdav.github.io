1232aec985fbabd8983feb0387ffacfda0a8f845
Preston Davis <prem.dav@outlook.com>
2021-08-19 20:07:30 -0500

    Removes scroll indicator for now, changes styling for text on about section

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => (
     <img className='avatar' src={avatar} alt='Profile Avatar' />
     <h1 className='name-headline'>Preston Davis</h1>
     <p className='quick-about'>Follower of Christ. Software Engineer. Coffee Enthusiast.</p>
-    <div className='icon-scroll'></div>
+    {/* <div className='icon-scroll'></div> */}
   </header>
 );
 
