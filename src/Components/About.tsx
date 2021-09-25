16d50fb8a78c29ac6849edf50402c27d366ca742
Preston Davis <prem.dav@outlook.com>
2021-09-25 04:48:25 -0700

    Makes api request to get github followers

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,51 +1,62 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import StatCard from './StatCard';
 
-const randomStats = [
-  {
-    mainInfo: '2.5',
-    minorInfo: 'Years of Professional Experience'
-  },
-  {
-    mainInfo: '2015',
-    minorInfo: 'Started Programming'
-  },
-  {
-    mainInfo: '8',
-    minorInfo: 'Github Followers'
-  },
-  {
-    mainInfo: 'React',
-    minorInfo: 'Favorite Framework/Library'
-  },
-  {
-    mainInfo: '6',
-    minorInfo: 'Countries Visited'
-  }
-];
-
-const renderCards = () => {
-  const cards = randomStats.map((stat) => <StatCard data={stat} />);
+const renderCards = (stats: any) => {
+  const cards = stats.map((stat: any) => <StatCard data={stat} />);
   return <>{cards}</>
 }
 
-const About = () => (
-  <section id='about' className='section'>
-    <h2 className='section-heading'>About</h2>
-    <div className='section-underline'></div>
-    <div className='section-content'>
-      <p>
-        Hey, I'm Preston! A software engineer, leading a team of 7, at Valcom, Inc. I work heavily in 
-        React, React Native, Node, and Python to build web and mobile applications for internal use and also for our emergency notification platforms.
-        When I am not working, I enjoy building out side projects, hanging out with family, and I absolutely love to travel.
-      </p>
-    </div>
-    <h2 className='section-heading'>Random Stats</h2>
-    <div className='section-underline'></div>
-    <div className='card-section'>
-      {renderCards()}
-    </div>
-  </section>
-);
+const About = () => {
+  const [ghFollowers, setGhFollowers] = useState(8);
+
+  const randomStats = [
+    {
+      mainInfo: '2.5',
+      minorInfo: 'Years of Professional Experience'
+    },
+    {
+      mainInfo: '2015',
+      minorInfo: 'Started Programming'
+    },
+    {
+      mainInfo: ghFollowers,
+      minorInfo: 'Github Followers'
+    },
+    {
+      mainInfo: 'React',
+      minorInfo: 'Favorite Framework/Library'
+    },
+    {
+      mainInfo: '6',
+      minorInfo: 'Countries Visited'
+    }
+  ];
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/premdav').then((res) => {
+      setGhFollowers(res.data.followers);
+    })
+  }, []);
+
+  return (
+    <section id='about' className='section'>
+      <h2 className='section-heading'>About</h2>
+      <div className='section-underline'></div>
+      <div className='section-content'>
+        <p>
+          Hey, I'm Preston! A software engineer, leading a team of 7, at Valcom, Inc. I work heavily in 
+          React, React Native, Node, and Python to build web and mobile applications for internal use and also for our emergency notification platforms.
+          When I am not working, I enjoy building out side projects, hanging out with family, and I absolutely love to travel.
+        </p>
+      </div>
+      <h2 className='section-heading'>Random Stats</h2>
+      <div className='section-underline'></div>
+      <div className='card-section'>
+        {renderCards(randomStats)}
+      </div>
+    </section>
+)
+};
 
 export default About;
