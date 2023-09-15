8875367fceefd4fc071e56aca9380b9eb40aa2b5
Preston Davis <premdav@Prestons-MBP.attlocal.net>
2023-09-15 11:01:19 -0500

    General maintenance and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,10 @@
-import React from 'react';
-import Particles from 'react-particles-js';
 import Header from './Components/Header';
-import About from './Components/About';
-import Projects from './Components/Projects';
 import Footer from './Components/Footer';
-
-const particleOptions = {
-  "particles": {
-      "number": {
-          "value": 100,
-          "density": {
-              "enable": true,
-              "value_area": 1500
-          }
-      },
-      "line_linked": {
-          "enable": true,
-          "opacity": 0.02
-      },
-      "move": {
-          "direction": "right",
-          "speed": 0.05
-      },
-      "size": {
-          "value": 1
-      },
-      "opacity": {
-          "anim": {
-              "enable": true,
-              "speed": 1,
-              "opacity_min": 0.1
-          }
-      }
-  },
-  "interactivity": {
-      "events": {
-          "onclick": {
-              "enable": true,
-              "mode": "push"
-          }
-      },
-      "modes": {
-          "push": {
-              "particles_nb": 1
-          }
-      }
-  },
-  "retina_detect": true
-}
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
-      <Particles className='particles'
-        params={particleOptions as any}
-      />
       <Header />
-      <About />
-      {/* <Projects /> */}
       <Footer />
     </div>
   );
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import avatar from '../assets/avataaars.png';
 
 const Header = () => (
@@ -6,7 +5,6 @@ const Header = () => (
     <img className='avatar' src={avatar} alt='Profile Avatar' />
     <h1 className='name-headline'>Preston Davis</h1>
     <p className='quick-about'>Follower of Christ. Software Engineer. Coffee Enthusiast.</p>
-    {/* <div className='icon-scroll'></div> */}
   </header>
 );
 
